a80272264553dcb12e55c71a3d953f385b50040c
chengyuesen <614280059@qq.com>
2021-07-13 10:55:29 +0800

    提交信息

---
+++ first_origram.py
@@ -0,0 +1,7 @@
+def sayhi(name):
+    print("hello world","name")
+    print("你好啊，初学git，请多指教")
+    print("second change")
+    print("hello world","name")
+    print("你好啊，初学git，请多指教")
+    print("second change")
